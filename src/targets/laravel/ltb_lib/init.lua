ca05a1498171d738d41c3c9ff83ad9472807b376
Eric Chiesse <echiesse@gmail.com>
2017-02-21 17:21:50 -0300

    Acerto do nome da lib que não havia sido atualizado.

--- src/targets/laravel/ltb_lib/init.lua
+++ src/targets/laravel/ltb_lib/init.lua
@@ -4,6 +4,6 @@ require "luno"
     luno.functional.exposeAll()
     luno.string.exposeSome()
 
-require "psetup_lib.DotEnvHelper"
-require "psetup_lib.util"
-require "psetup_lib.db_utils"
+require "ltb_lib.DotEnvHelper"
+require "ltb_lib.util"
+require "ltb_lib.db_utils"
