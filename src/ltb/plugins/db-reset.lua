810b8a2eedd974ddc2e335a1ccfe3ac6ddab73ec
Eric Chiesse <echiesse@gmail.com>
2018-04-02 17:05:12 -0300

    Acertado o comando db-reset. Alterado o comando db-restore para usar o db-reset como dependência.

--- src/ltb/plugins/db-reset.lua
+++ src/ltb/plugins/db-reset.lua
@@ -1,42 +1,35 @@
-require "ltb_lib"
 
---##############################################################################
+local plugin = 
+{
+    name = "db-reset"
+}
 
 
-local plugin = {}
-
-
-function plugin.exec(databaseName, backupFile)
-    local params = [[--protocol=tcp --host=127.0.0.1 --user=root --port=3307 --default-character-set=utf8 --comments]]
-    local baseSqlCommand = lstring.joinWords{MY_SQL_CMD, params}
-
-    databaseName = databaseName or dotEnv:get("DB_DATABASE")
-    backupFile = backupFile or dbBackupName(databaseName)
-
+function plugin.exec(target, db, databaseName)
+    databaseName = databaseName or target.getDatabaseName()
     if databaseName == nil then
-        print("O banco de dados alvo deve ser especificado na linha de comando ou no arquivo .env")
+        print("O banco de dados alvo deve ser especificado na linha de comando ou no arquivo de configuração")
         os.exit(0)
     end
-    print("Apagando tabelas da base de dados '" .. databaseName .. "'")
+
+    print("Reiniciando base de dados '" .. databaseName .. "'")
+    local sqlShellCommand = db.getShellCommand()
 
     -- Obter todas as tabelas do DB:
-    local sqlCommand = string.format([[echo show tables from %s | %s]], databaseName, baseSqlCommand)
+    local sqlCommand = db.listTablesCommand(databaseName)
     os.execute(sqlCommand .. "> output.out")
     local tables = filter(function(x) return trim(x) ~= "" end, drop(1, lstring.splitLines(lio.getTextFromFile("output.out"))))
-
 
     -- Apagar as tabelas obtidas:
     print("Apagando tabelas atuais")
     for i, table in ipairs(tables) do
-        local dropCommand = require("psetup_lib.dropTableCmdTemplate")
+        local dropCommand = db.dropTableCmdTemplate
         dropCommand = dropCommand:format(databaseName, table)
         local dropCommandFile = "drop.sql"
         lio.saveTextToFile(dropCommand, dropCommandFile)
-        os.execute(baseSqlCommand .. " < " .. dropCommandFile .. " > output.out")
+        os.execute(sqlShellCommand .. " < " .. dropCommandFile .. " > output.out")
         os.remove(dropCommandFile)
-        print("Apagada tabela: " .. table)
     end
-    print("")
     print("Tabelas apagadas com sucesso")
 
 end
--- src/ltb/plugins/db-restore.lua
+++ src/ltb/plugins/db-restore.lua
@@ -1,3 +1,4 @@
+local dbReset = require "ltb.plugins.db-reset"
 
 local plugin =
 {
@@ -6,35 +7,11 @@ local plugin =
 
 
 function plugin.exec(target, db, databaseName, backupFile)
-    
-    databaseName = databaseName or target.getDatabaseName()
-    if databaseName == nil then
-        print("O banco de dados alvo deve ser especificado na linha de comando ou no arquivo de configuração")
-        os.exit(0)
-    end
 
-    print("Atualizando base de dados '" .. databaseName .. "'")
-    local sqlShellCommand = db.getShellCommand()
-
-    -- Obter todas as tabelas do DB:
-    local sqlCommand = db.listTablesCommand(databaseName)
-    os.execute(sqlCommand .. "> output.out")
-    local tables = filter(function(x) return trim(x) ~= "" end, drop(1, lstring.splitLines(lio.getTextFromFile("output.out"))))
-
-    -- Apagar as tabelas obtidas:
-    print("Apagando tabelas atuais")
-    for i, table in ipairs(tables) do
-        local dropCommand = db.dropTableCmdTemplate
-        dropCommand = dropCommand:format(databaseName, table)
-        local dropCommandFile = "drop.sql"
-        lio.saveTextToFile(dropCommand, dropCommandFile)
-        os.execute(sqlShellCommand .. " < " .. dropCommandFile .. " > output.out")
-        os.remove(dropCommandFile)
-    end
-    print("Tabelas apagadas")
+    dbReset.exec(target, db, databaseName)
 
     -- Importar Dados:
-    backupFile = backupFile or databaseName .. "_dump.sql"
+    backupFile = backupFile or table.concat({config.DB_BACKUP_DIR,  databaseName .. "_dump.sql"}, DIR_SEP)
     sqlCommand = sqlShellCommand .. [[ --database=%s < ]] .. backupFile
     sqlCommand = sqlCommand:format(databaseName)
     print("Importando dados do arquivo '" .. backupFile .. "'")
